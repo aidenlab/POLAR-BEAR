a7f4b6a2e2e3f9f90a06bc51343ca3b5252612e4
nchernia <nchernia@gmail.com>
2020-05-21 16:39:34 -0400

    debug

--- align_slurm.sh
+++ align_slurm.sh
@@ -292,7 +292,7 @@ echo "#SBATCH --threads-per-core=1 " >> "$WORK_DIR"/collect_stats.sh
 echo "$slurm_depend_merge"  >> "$WORK_DIR"/collect_stats.sh 
 echo "echo \"label,percentage\" > $WORK_DIR/stats.csv " >> "$WORK_DIR"/collect_stats.sh
 echo "for f in $WORK_DIR/*/aligned/depth_per_base.txt; do"  >> "$WORK_DIR"/collect_stats.sh
-echo "if [[ ! \"$f\" == *\"$control\"* ]]; then" >> "$WORK_DIR"/collect_stats.sh 
+echo "if [[ ! \"\$f\" == *\"control\"* ]]; then" >> "$WORK_DIR"/collect_stats.sh 
 echo  "awk -v fname=\$(basename \${f%%/aligned*}) 'BEGIN{count=0; onisland=0}\$3>5{if (!onisland){onisland=1; island_start=\$2}}\$3<=5{if (onisland){island_end=\$2; if (island_end-island_start>=50){count=count+island_end-island_start}} onisland=0}END{if (onisland){island_end=\$2; if (island_end-island_start>=50){count=count+island_end-island_start}}  if (NR==0){NR=1} printf(\"%s,%0.02f\n\", fname, count*100/NR)}' \$f >> ${WORK_DIR}/stats.csv"  >> "$WORK_DIR"/collect_stats.sh 
 echo "fi;	done "  >> "$WORK_DIR"/collect_stats.sh
 
